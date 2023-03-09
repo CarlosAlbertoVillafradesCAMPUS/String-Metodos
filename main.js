76772681723ad556b8a697b81c5c88dee51cbb1a
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-08 19:46:59 -0500

    feat:[METODOS-STRING]🚀 Metodo .fromCharCode()
    Explicacion con ejemplo sobre el metodo .fromCharCode()
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-//.charCodeAt(index)
+//.fromCharCode(num,num,...)
 
-console.log(".charCodeAt(index)")
+console.log(".fromCharCode(num,num,...)")
 
-let oracion = "los mejores programadores esta en campus";
-console.log(`oracion = ${oracion} `)
+console.log(`String.fromCharCode(65) = ${String.fromCharCode(65)}`)
+console.log(`String.fromCharCode(66) = ${String.fromCharCode(66)}`)
+console.log(`String.fromCharCode(67) = ${String.fromCharCode(67)}`)
 
-console.log(`oracion.charCodeAt(4) = ${oracion.charCodeAt(4)}`)
-console.log(`oracion.charCodeAt(5) = ${oracion.charCodeAt(5)}`)
-console.log(`oracion.charCodeAt(6) = ${oracion.charCodeAt(6)}`)
-console.log(`oracion.charCodeAt(7) = ${oracion.charCodeAt(7)}`)
-console.log(`oracion.charCodeAt(8) = ${oracion.charCodeAt(8)}`)
-console.log(`oracion.charCodeAt(9) = ${oracion.charCodeAt(9)}`)
+
+
+// let oracion = "los mejores programadores esta en campus";
+// console.log(`oracion = ${oracion} `)
+
+// console.log(`oracion.charCodeAt(4) = ${oracion.charCodeAt(4)}`)
+// console.log(`oracion.charCodeAt(5) = ${oracion.charCodeAt(5)}`)
+// console.log(`oracion.charCodeAt(6) = ${oracion.charCodeAt(6)}`)
+// console.log(`oracion.charCodeAt(7) = ${oracion.charCodeAt(7)}`)
+// console.log(`oracion.charCodeAt(8) = ${oracion.charCodeAt(8)}`)
+// console.log(`oracion.charCodeAt(9) = ${oracion.charCodeAt(9)}`)
