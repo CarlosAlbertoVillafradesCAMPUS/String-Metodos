037bb62e689b1c406052a8c0d397f4758e41f931
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-08 19:51:06 -0500

    feat:[METODOS-STRING]🚀 Metodo .concat()
    Explicacion con ejemplo sobre el metodo .concat()
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-//.fromCharCode(num,num,...)
+//.concat(str1,str2)
 
-console.log(".fromCharCode(num,num,...)")
+console.log(".concat(str1,str2)")
 
-console.log(`String.fromCharCode(65) = ${String.fromCharCode(65)}`)
-console.log(`String.fromCharCode(66) = ${String.fromCharCode(66)}`)
-console.log(`String.fromCharCode(67) = ${String.fromCharCode(67)}`)
+let oracion1 = "los mejores programadores";
+let oracion2 = "estan en campus";
+console.log(`oracion1 = ${oracion1} `)
+console.log(`oracion2 = ${oracion2} `)
 
+console.log(`oracion1.concat(' ', oracion2) = ${oracion1.concat(' ', oracion2)}`)
 
-
-// let oracion = "los mejores programadores esta en campus";
-// console.log(`oracion = ${oracion} `)
-
-// console.log(`oracion.charCodeAt(4) = ${oracion.charCodeAt(4)}`)
-// console.log(`oracion.charCodeAt(5) = ${oracion.charCodeAt(5)}`)
-// console.log(`oracion.charCodeAt(6) = ${oracion.charCodeAt(6)}`)
-// console.log(`oracion.charCodeAt(7) = ${oracion.charCodeAt(7)}`)
-// console.log(`oracion.charCodeAt(8) = ${oracion.charCodeAt(8)}`)
-// console.log(`oracion.charCodeAt(9) = ${oracion.charCodeAt(9)}`)
