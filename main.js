c7f930de25c9db1776c8d86578bb3b3bae604069
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-08 19:35:22 -0500

    feat:[METODOS-STRING]🚀 Metodo .charAt()
    Explicacion con ejemplo sobre el metodo .charAt()
    
    by:Carlos Villafrades

---
+++ main.js
@@ -0,0 +1,13 @@
+//.charAt(indice)
+
+console.log(".charAt(indice)")
+
+let oracion = "los mejores programadores esta en campus";
+console.log(`oracion = ${oracion} `)
+
+console.log(`oracion.charAt(4) = ${oracion.charAt(4)}`)
+console.log(`oracion.charAt(5) = ${oracion.charAt(5)}`)
+console.log(`oracion.charAt(6) = ${oracion.charAt(6)}`)
+console.log(`oracion.charAt(7) = ${oracion.charAt(7)}`)
+console.log(`oracion.charAt(8) = ${oracion.charAt(8)}`)
+console.log(`oracion.charAt(9) = ${oracion.charAt(9)}`)
